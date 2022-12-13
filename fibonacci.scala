7544a803a096d888f1c77883930c7f20412827bf
rupaliT123 <117934863+rupaliT123@users.noreply.github.com>
2022-12-13 17:05:58 +0530

    Add files via upload

--- fibonacci.scala
+++ fibonacci.scala
@@ -4,7 +4,7 @@ package training.practice
 //Write program for fibonacci series
 object fibonacci {
    def main(args:Array[String]):Unit={
-     val num :Int = scala.io.StdIn.readInt()
+     var num :Int = scala.io.StdIn.readInt()
      var first_number:Int = 0
      var second_number :Int = 1
      var number:Int = 0
@@ -16,6 +16,27 @@ object fibonacci {
          second_number = number
        }
        print(first_number)
+
+     println("\nUsing Recursion----")
+
+     val f :Int = 0
+     val s :Int = 1
+     var number1 :Int = num
+     fibbonacci(f,s,number1)
+     println("\nDone")
+     println("Using Tail Recursion -----")
    }
+
+  def fibbonacci(first: Int, second: Int,number1:Int) :Int = {
+    if(number1 == 0)
+      {
+        return 0
+      }
+    print( first+" ")
+
+    var num : Int = first + second
+    var num1 = number1 - 1;
+    fibbonacci(second, num,num1)
+  }
 
 }
