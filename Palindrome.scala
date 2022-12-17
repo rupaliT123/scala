622532d1772236064994fb2ff330bfe2a7989187
rupaliT123 <117934863+rupaliT123@users.noreply.github.com>
2022-12-17 11:09:38 +0530

    Update Palindrome.scala

--- Palindrome.scala
+++ Palindrome.scala
@@ -5,11 +5,15 @@ import scala.annotation.tailrec
 
 object Palindrome {
   def main(args:Array[String]):Unit = {
-    val list1:List[Int] = List(1,2,3,2,1)
+    val list1:List[Int] = List(5,2,4,5,2,5)
     val list2:List[Int] = List(1,2,3,4,1)
+    val list3:List[Int] = list1.reverse
     var i:Int = 0
     var j:Int = list1.size-1
     var isPalindrome:Boolean = true
+
+    //Achieving immutability with tail recursion
+    println("Immutable :"+palindromeCheck2(list1,list3,true))
 
     // Using tail recursion for list1
     if (list1.size == 1) {
@@ -29,13 +33,23 @@ object Palindrome {
     else {
       println("Given list2 is palindrome :" + palindromeCheck1(list2, i, k, isPalindrome))
     }
-
   }
 
+  def palindromeCheck2(list1: List[Int], list2: List[Int], accum1: Boolean):Boolean ={
+   @tailrec
+    def checkIt(accum1:Boolean,list1:List[Int],list2:List[Int]):Boolean ={
+     accum1 match {
+       case false => false
+       case _ if(list1.isEmpty | list2.isEmpty) => true
+       case true => checkIt(list1.head == list2.head ,list1.tail,list2.tail)
+     }
+    }
+    checkIt(accum1,list1, list2)
+  }
   @tailrec
   def palindromeCheck(list: List[Int], i: Int, j: Int, isPalindrome: Boolean): Boolean = {
     if (i >= j) return isPalindrome
-    else if (isPalindrome == false) return isPalindrome
+    else if (isPalindrome == false)  return isPalindrome
     if (list(i) != list(j)) {
       palindromeCheck(list, i + 1, j - 1, false)
     }
