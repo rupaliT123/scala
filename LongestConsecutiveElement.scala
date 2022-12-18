6feed9c7648eb82b801b7c8a01fffaea5247cbf3
rupaliT123 <117934863+rupaliT123@users.noreply.github.com>
2022-12-18 16:26:42 +0530

    Update LongestConsecutiveElement.scala

--- LongestConsecutiveElement.scala
+++ LongestConsecutiveElement.scala
@@ -1,19 +1,27 @@
 package com.techsophy
 package training.practice
 
+import com.techsophy.training.practice.LongestConsecutiveElement.LongestSequenceInfo
+
 import scala.annotation.tailrec
 
 object LongestConsecutiveElement {
   def main(args:Array[String]):Unit = {
-    val list:List[Int] =  List(1,2,3,3,4,4,4)
+    val list:List[Int] =  List(-1,-1,-1,0,0,0,1,2,3,3,3,4,4,4,5,5)
     if(list.isEmpty) println("Given list is empty")
-    else println(longestConsecutive(list.head,list.head,1,1,list.tail))
+    else {
+      val c = longestConsecutive(list.head, list.head, 1, 1, list.tail)
+      println("Element: "+c.element)
+      println("Frequency: "+c.frequency)
+    }
   }
+  case class LongestSequenceInfo(element: Int, frequency: Int)
+
   @tailrec
-  def longestConsecutive(finalElement: Int, element: Int, frequency: Int, finalFrequency: Int, list: List[Int]):(Int,Int)={
+  def longestConsecutive(finalElement: Int, element: Int, frequency: Int, finalFrequency: Int, list: List[Int]):LongestSequenceInfo={
     if(list.isEmpty)
       {
-        return (finalElement,finalFrequency)
+        return LongestSequenceInfo(finalElement,finalFrequency)
       }
     if(element == list.head)
       {
